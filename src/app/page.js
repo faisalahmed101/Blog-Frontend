46a93478b0aefbee3f95d527a6f3ac38b40e5a5f
Faisal <faisalahmed4315@gmail.com>
2024-11-09 13:51:41 +0600

    test

--- src/app/page.js
+++ src/app/page.js
@@ -39,6 +39,10 @@ export default function Home() {
         />
       </div>
 
+      <div>
+        <h1>this is home page</h1>
+      </div>
+
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
